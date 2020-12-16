29b516233e42ab3a31a85ce2aad9b48fcbb7c1e4
Vishal Sengar <vishalsengardtu@gmail.com>
2020-12-16 17:06:04 +0530

    Express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express') 
+const path = require('path')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`)
+}) 
